53211fb79385ffaa87a0cbd5251b344e230cc003
Davinsry <davinipad53@gmail.com>
2025-10-30 11:39:39 +0700

    Menambah @Composable fun FormDataDiri(modifier: Modifier ){     // variabel-variabel untuk mengingat nilai masukan dari keyboard     var textNama by remember { mutableStateOf("") }     var textAlamat by remember { mutableStateOf(" ") }     var textJK by remember { mutableStateOf("") }
        // variabel-variabel untuk menyimpan data yang diperoleh dari komponen UI
        var nama by remember { mutableStateOf("") }
        var alamat by remember { mutableStateOf(" ") }
        var jenis by remember { mutableStateOf("") }
    }

--- app/src/main/java/com/example/inputpengguna/TeksEdit.kt
+++ app/src/main/java/com/example/inputpengguna/TeksEdit.kt
@@ -1,8 +1,22 @@
 package com.example.inputpengguna
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 
 @Composable
 fun FormDataDiri(modifier: Modifier
-)
+){
+    // variabel-variabel untuk mengingat nilai masukan dari keyboard
+    var textNama by remember { mutableStateOf("") }
+    var textAlamat by remember { mutableStateOf(" ") }
+    var textJK by remember { mutableStateOf("") }
+
+    // variabel-variabel untuk menyimpan data yang diperoleh dari komponen UI
+    var nama by remember { mutableStateOf("") }
+    var alamat by remember { mutableStateOf(" ") }
+    var jenis by remember { mutableStateOf("") }
+}
