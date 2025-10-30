91bd9cda718bc3f2d9b648f71055c5667fb8c945
Davinsry <davinipad53@gmail.com>
2025-10-30 11:59:49 +0700

    Menambah Row Row {                          gender.forEach { item ->                              Row(modifier = Modifier.selectable(                                  selected = textJK == item,                                  onClick = { textJK = item }                              ), verticalAlignment = Alignment.CenterHorizontally as Alignment.Vertical) {                                  RadioButton(                                      selected = textJK == item,                                      onClick = {                                          textJK = item                                      })
                                 }
                             }
                         }

--- app/src/main/java/com/example/inputpengguna/TeksEdit.kt
+++ app/src/main/java/com/example/inputpengguna/TeksEdit.kt
@@ -1,13 +1,16 @@
 package com.example.inputpengguna
 
 import android.R
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -47,7 +50,19 @@ fun FormDataDiri(modifier: Modifier
             }
         )
         Row {
+            gender.forEach { item ->
+                Row(modifier = Modifier.selectable(
+                    selected = textJK == item,
+                    onClick = { textJK = item }
+                ), verticalAlignment = Alignment.CenterHorizontally as Alignment.Vertical) {
+                    RadioButton(
+                        selected = textJK == item,
+                        onClick = {
+                            textJK = item
+                        })
 
+                }
+            }
         }
     }
 
